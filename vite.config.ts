9de7a0797ea1941bb0ea312af71a994ddc144a04
Maël <chanon.mael@gmail.com>
2024-08-04 00:45:37 +0200

    changed header

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/portefolio/',
+  server: {
+    headers: {
+      'Permissions-Policy': 'interest-cohort=()', // Disallow interest-cohort
+    },
+  },
 })
